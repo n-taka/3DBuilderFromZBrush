018941ae23f503a9934b15bcadc530b2be3276b9
Kazutaka Nakashima <kazutaka.nakashima@n-taka.info>
2020-08-11 21:38:32 +0900

    [impl] simple drag-and-drop

--- main.cpp
+++ main.cpp
@@ -1,32 +1,48 @@
 
 #include <vector>
 #include <string>
+#include <filesystem>
 #include "readGoZFile.h"
 #include "repairAs3DBuilder.h"
 #include "writeGoZFile.h"
 
-void main()
+void main(int argc, char* argv[])
 {
-    std::string meshName;
-    std::vector<std::vector<double>> V, VC;
-    std::vector<double> M;
-    std::vector<int> G;
-    std::vector<std::vector<int>> F;
-    std::vector<std::vector<std::pair<double, double>>> UV;
+    if (argc >= 2)
+    {
+        std::string inputGoZFileName;
+        std::string meshName;
+        std::vector<std::vector<double>> V, VC;
+        std::vector<double> M;
+        std::vector<int> G;
+        std::vector<std::vector<int>> F;
+        std::vector<std::vector<std::pair<double, double>>> UV;
 
-    FromZ::readGoZFile("./example/Dog.GoZ", meshName, V, F, UV, VC, M, G);
+        inputGoZFileName = std::string(argv[1]);
 
-    std::vector<std::vector<double>> V_repair, VC_repair;
-    std::vector<double> M_repair;
-    std::vector<int> G_repair;
-    std::vector<std::vector<int>> F_repair;
-    std::vector<std::vector<std::pair<double, double>>> UV_repair;
+        FromZ::readGoZFile(inputGoZFileName, meshName, V, F, UV, VC, M, G);
 
-    repairAs3DBuilder(V, F, UV, VC, M, G, V_repair, F_repair, UV_repair, VC_repair, M_repair, G_repair);
+        std::string outputGoZFileName;
+        std::vector<std::vector<double>> V_repair, VC_repair;
+        std::vector<double> M_repair;
+        std::vector<int> G_repair;
+        std::vector<std::vector<int>> F_repair;
+        std::vector<std::vector<std::pair<double, double>>> UV_repair;
 
-    FromZ::writeGoZFile("./example/DogNotRepairOut.GoZ", meshName, V, F, UV, VC, M, G);
-    FromZ::writeGoZFile("./example/DogRepairOut.GoZ", meshName, V_repair, F_repair, UV_repair, VC_repair, M_repair, G_repair);
+        repairAs3DBuilder(V, F, UV, VC, M, G, V_repair, F_repair, UV_repair, VC_repair, M_repair, G_repair);
 
+        std::filesystem::path input(inputGoZFileName);
+        std::filesystem::path output = input.parent_path();
+        output /= input.stem();
+        output += "_repaired.GoZ";
+        outputGoZFileName = output.string();
+
+        FromZ::writeGoZFile(outputGoZFileName, meshName, V_repair, F_repair, UV_repair, VC_repair, M_repair, G_repair);
+    }
+    else
+    {
+        std::cerr << "Please drag-and-drop GoZ file to this .exe file." << std::endl;
+    }
 
     return;
 }
